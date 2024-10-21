71c678cac1fbcf5ef686b5635a8f0fec289deb23
Bhanyu <whohuseynovabhanyu10@gmail.com>
2024-10-21 12:20:34 +0400

    final3

--- pos-website/src/App.js
+++ pos-website/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Route, Routes } from "react-router-dom";
 import Layout from '../src/components/Layout/Layout'
 import Home from '../src/pages/Home/Home';
 import SingleSector from './pages/Home/SingleSector';
