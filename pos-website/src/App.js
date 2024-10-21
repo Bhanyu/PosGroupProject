38f915a0a781567d9ef24ef43ef6458f99615d85
Bhanyu <whohuseynovabhanyu10@gmail.com>
2024-10-21 23:11:57 +0400

    final4

--- pos-website/src/App.js
+++ pos-website/src/App.js
@@ -1,9 +1,44 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+// // import { Routes, Route } from 'react-router-dom';
+// import { HashRouter as Router,Route, Routes } from "react-router-dom";
+// import Layout from '../src/components/Layout/Layout'
+// import Home from '../src/pages/Home/Home';
+// import SingleSector from './pages/Home/SingleSector';
+// import About from './pages/About/About';
+// import Partners from './pages/Partners/Partners';
+// import Sektors from './pages/Sectors/Sektors';
+// import Equipments from './pages/Equipments/Equipments';
+// import Software from './pages/Software/Software';
+// import Connect from './pages/Connect/Connect';
+// import Bags from './pages/Bag/Bags';
+// function App() {
+//   return (
+//     <>
+//       <Routes>
+//         <Route path="/" element={<Layout />}>
+//           <Route index element={<Home />} />
+//           <Route path="sectors/:sectorsId" element={<SingleSector />} />
+//           <Route path="about" element={<About/>} />
+//           <Route path="partners" element={<Partners/>} />
+//           <Route path="sectors" element={<Sektors/>} />
+//           <Route path="equipments" element={<Equipments/>} />
+//           <Route path="software" element={<Software/>} />
+//           <Route path="connection" element={<Connect/>} />
+//           <Route path="bags" element={<Bags/>} />
+//         </Route>
+//       </Routes>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
 import './App.css';
-// import { Routes, Route } from 'react-router-dom';
-import { HashRouter as Route, Routes } from "react-router-dom";
-import Layout from '../src/components/Layout/Layout'
-import Home from '../src/pages/Home/Home';
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home/Home';
 import SingleSector from './pages/Home/SingleSector';
 import About from './pages/About/About';
 import Partners from './pages/Partners/Partners';
@@ -12,25 +47,27 @@ import Equipments from './pages/Equipments/Equipments';
 import Software from './pages/Software/Software';
 import Connect from './pages/Connect/Connect';
 import Bags from './pages/Bag/Bags';
+
 function App() {
   return (
-    <>
+    <Router> {/* Wrapping the routes inside HashRouter */}
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="sectors/:sectorsId" element={<SingleSector />} />
-          <Route path="about" element={<About/>} />
-          <Route path="partners" element={<Partners/>} />
-          <Route path="sectors" element={<Sektors/>} />
-          <Route path="equipments" element={<Equipments/>} />
-          <Route path="software" element={<Software/>} />
-          <Route path="connection" element={<Connect/>} />
-          <Route path="bags" element={<Bags/>} />
+          <Route path="about" element={<About />} />
+          <Route path="partners" element={<Partners />} />
+          <Route path="sectors" element={<Sektors />} />
+          <Route path="equipments" element={<Equipments />} />
+          <Route path="software" element={<Software />} />
+          <Route path="connection" element={<Connect />} />
+          <Route path="bags" element={<Bags />} />
         </Route>
       </Routes>
-    </>
+    </Router>
   );
 }
 
 export default App;
+
 
--- pos-website/src/index.js
+++ pos-website/src/index.js
@@ -1,11 +1,27 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+// import App from './App';
+
+// import { BrowserRouter } from 'react-router-dom';
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <BrowserRouter>
+//     <App />
+//     </BrowserRouter>
+// )
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import './index.css';
 import App from './App';
+import { HashRouter as Router } from 'react-router-dom'; // Import HashRouter
 
-import { BrowserRouter } from 'react-router-dom';
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-    </BrowserRouter>
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router> {/* Wrapping the App component in HashRouter */}
+      <App />
+    </Router>
+  </React.StrictMode>
+);
